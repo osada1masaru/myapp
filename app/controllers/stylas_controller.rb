84a8a9f71d48d1417b0b8f0925a058a95aa0fea7
osada1masaru <masaru5151su@icloud.com>
2023-10-13 09:22:09 +0900

    [add]devise gem導入、初期設定済みページ確認済み。

---
+++ app/controllers/stylas_controller.rb
@@ -0,0 +1,7 @@
+class StylasController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'stylas/index'
+  get 'stylas/show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
 end
