09ab1dda9bf38e373f2216bf219354c4b59dba50
osada1masaru <masaru5151su@icloud.com>
2023-10-13 12:03:56 +0900

    [upadte]ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :styles
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get '/users/sign_up' => 'devise/registrations#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
