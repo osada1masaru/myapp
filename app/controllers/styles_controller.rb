3a937558c8aaa925a9eee0f687f3368cd427a212
osada1masaru <masaru5151su@icloud.com>
2023-10-14 16:32:08 +0900

    [update]スタイル投稿フォーム作成

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,13 +1,45 @@
 class StylesController < ApplicationController
   def index
+    @styles = Style.wuth_attached_image
   end
 
   def show
+    @style = Atyle.find(params[:id])
   end
 
   def new
+    @style = Style.new
   end
 
-  def edit
+  def edit; end
+
+  def create
+    @style = current_user.styles.build(style_params)
+
+    if @style.save
+      redirect_to @style, nitice: "スタイル投稿完了"
+    else
+      flash.now[:alert] = "スタイルの投稿に失敗"
+      render :new
+    end
+  end
+
+  def update
+    if @style.update(style_params)
+      redirect_to @style, notice: "スタイルの更新完了"
+    else
+      flash.now[:alert] = "スタイルの更新に失敗"
+      render :edit
+    end
+  end
+
+  def destory
+    if @style.destory
+      flash[:notice] = "スタイルが削除されました"
+    else
+      flash[:alert] = "スタイルの削除に失敗"
+    end
+
+    render_to styles_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  has_one_attached :image
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_many :styles
 end
